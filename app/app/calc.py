1c9a11b807ec19f49626fe4829ad0df94d92d7de
Abdisalan Mohamed <abdisalanvp77@gmail.com>
2026-04-14 08:01:49 -0600

    More unit tests using tdd

--- app/app/calc.py
+++ app/app/calc.py
@@ -13,3 +13,36 @@ def add(x, y):
         _type_: _description_ the sum of x and y
     """
     return x + y
+def subtract(x, y):
+    """_summary_ subtract two numbers
+
+    Args:
+        x (_type_): _description_ an integer
+        y (_type_): _description_ an integer
+
+    Returns:
+        _type_: _description_ the difference of x and y
+    """
+    return x - y
+def multiply(x, y):
+    """_summary_ multiply two numbers
+
+    Args:
+        x (_type_): _description_ an integer
+        y (_type_): _description_ an integer
+
+    Returns:
+        _type_: _description_ the product of x and y
+    """
+    return x * y
+def divide(x, y):
+    """_summary_ divide two numbers
+
+    Args:
+        x (_type_): _description_ an integer
+        y (_type_): _description_ an integer
+
+    Returns:
+        _type_: _description_ the quotient of x and y
+    """
+    return x / y
--- app/app/tests.py
+++ app/app/tests.py
@@ -12,3 +12,19 @@ class CalcTests(SimpleTestCase):
         """
         res = calc.add(2, 3)
         self.assertEqual(res, 5)
+        
+    def test_subtract(self):
+        """_summary_ Test the subtract function.
+        """
+        res = calc.subtract(5, 2)
+        self.assertEqual(res, 3)
+    def test_multiply(self):
+        """_summary_ Test the multiply function.
+        """
+        res = calc.multiply(2, 3)
+        self.assertEqual(res, 6)
+    def test_divide(self):
+        """_summary_ Test the divide function.
+        """
+        res = calc.divide(6, 2)
+        self.assertEqual(res, 3)
