63f7b3cf70b32d2e45aa455172767e03d73ec7c2
Abdisalan Mohamed <abdisalanvp77@gmail.com>
2026-04-15 12:52:17 -0600

    Register the custom user model with the admin site.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin  # noqa: F401
+"""_summary_
+Django admin modifications.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        (
+            'Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)  # Register the custom user model with the admin site.
