b9daec981fa5ea6abf1f1e91e7b548468bf52f7d
Abdisalan Mohamed <abdisalanvp77@gmail.com>
2026-04-15 22:54:43 -0600

    updated the private api user management endpoints

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,21 +1,25 @@
 """_summary_
 Serializers for the user API View
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate
+)
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 # Get the user model defined in the core app,
 # which is a custom user model that extends AbstractBaseUser and PermissionsMixin.
 # This allows us to use the custom user model in our serializers and views
 # without having to import it directly from the core app.
-User = get_user_model()
+# by using get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('email', 'password', 'name')
         extra_kwargs = {
             'password': {
@@ -26,4 +30,41 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Create a new user."""
-        return User.objects.create_user(**validated_data)
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it."""
+        password = validated_data.pop('password', None)  # remove the password from the validated data, if it exists, so that it is not updated as a regular field
+        user = super().update(instance, validated_data)  # call the parent class's update method to update the user instance with the validated data, excluding the password
+
+        if password:
+            user.set_password(password)  # if a password was provided, set it using the set_password method, which will hash the password and save it to the database
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,46 @@
 """_summary_
 Views for the user API.
 """
-from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework import generics, permissions, authentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
+    # we use the UserSerializer for the CreateUserView,
+    # since it handles the creation of a new user correctly,
+    # including hashing the password and validating the email field
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for the user."""
+    # we use a custom serializer for the token view, since the default one does not include the email field,
+    # which is required for our custom user model
+    serializer_class = AuthTokenSerializer
+    # we set the renderer classes to the default renderer classes, which includes the JSONRenderer,
+    # so that we can get a JSON response when we create a token,
+    # instead of the default browsable API response, which is not suitable for our use case
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    # we use the same serializer for creating and updating the user,
+    # since it handles both cases correctly
+    serializer_class = UserSerializer
+    # we use token authentication for this view, since we want to authenticate the user using a token,
+    # and we want to ensure that only authenticated users can access this view
+    authentication_classes = [authentication.TokenAuthentication]
+    # we set the permission classes to IsAuthenticated,
+    # since we want to ensure that only authenticated users can access this view
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
