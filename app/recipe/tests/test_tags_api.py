63aba68a766bfedda2edb63dbc572b564d71b91b
Abdisalan Mohamed <abdisalanvp77@gmail.com>
2026-04-16 19:50:57 -0600

    CRUD unit tests of the tags api

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -12,6 +12,11 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Return tag detail URL."""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
@@ -40,6 +45,7 @@ class PrivateTagsApiTests(TestCase):
             password='testpass123'
         )
         self.client.force_authenticate(user=self.user)
+
     def test_retrieve_tags(self):
         """Test retrieving tags."""
         Tag.objects.create(user=self.user, name='Vegan')
@@ -68,11 +74,68 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
+
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
+
+    def test_delete_other_users_tag_error(self):
+        """Test trying to delete another user's tag gives error."""
+        other_user = create_user(
+            email='other@example.com',
+            password='testpass123'
+        )
+        tag = Tag.objects.create(user=other_user, name='Other User Tag')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
+        tags = Tag.objects.filter(user=other_user)
+        self.assertTrue(tags.exists())
+
+    def test_update_other_users_tag_error(self):
+        """Test trying to update another user's tag gives error."""
+        other_user = create_user(
+            email='other@example.com',
+            password='testpass123'
+        )
+        tag = Tag.objects.create(user=other_user, name='Other User Tag')
+
+        payload = {'name': 'Updated Name'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, 'Other User Tag')
+
     def test_create_tag_successful(self):
         """Test creating a new tag."""
-        payload = {'name': 'Test tag'}
-        self.client.post(TAGS_URL, payload)
+        payload = {'name': 'Test Tag'}
+        res = self.client.post(TAGS_URL, payload)
 
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         exists = Tag.objects.filter(
             user=self.user,
             name=payload['name']
@@ -85,3 +148,8 @@ class PrivateTagsApiTests(TestCase):
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertFalse(exists)
