b6e6bef08b3c50a65c86598b68a47e9e288da9d4
Abdisalan Mohamed <abdisalanvp77@gmail.com>
2026-04-16 19:52:38 -0600

    Implement crud functionality making use of the built in mixins

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,17 @@
 """
 Views for the recipe APIs
 """
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag
+)
 from recipe import serializers
 
 
@@ -31,4 +37,38 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return serializers.RecipeSerializer
 
+        return self.serializer_class
+
+
+# The TagViewSet is a viewset that provides list and
+# create actions for the Tag model.
+# It uses the TagSerializer to serialize the data and
+# requires token authentication and that the user is authenticated.
+# The get_queryset method filters the tags to return only those that belong
+# to the current authenticated user, and the perform_create method sets the
+# user to the current authenticated user when creating a new tag.
+class TagViewSet(
+                mixins.DestroyModelMixin,
+                mixins.CreateModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.ListModelMixin,
+                viewsets.GenericViewSet):
+    """View for manage tag APIs
+    """
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        # Set the user to the current authenticated user when creating a new tag
+        serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
         return self.serializer_class
