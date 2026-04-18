6839b772d12ada3bd0d3290ea853ceb526624619
Abdisalan Mohamed <abdisalanvp77@gmail.com>
2026-04-17 20:49:06 -0600

    implemented tag and ingredient filtering

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -1,11 +1,10 @@
 """Tests for the ingredients API."""
-from decimal import Decimal
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
-from core.models import Ingredient, Recipe, Tag
+from core.models import Ingredient, Recipe
 from recipe.serializers import IngredientSerializer
 
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
